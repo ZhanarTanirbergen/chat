b6c3bd11b08d73211cfa417874ec6529be5b419a
ZhanarTanirbergen <zhanara.tanirbergenova@gmail.com>
2017-04-07 19:05:48 +0600

    improved
    new functionality was added + bugs were fixed

--- script.js
+++ script.js
@@ -81,7 +81,6 @@
     	$('label').remove('#messageAuthor');
     	$('label').remove('#messageText');
     	var aMessages = messageManager.getMessagesList();
-    	console.log(aMessages);
 	    for(var i = 0; i < aMessages.length; i++) {
 	    	if((aMessages[i].receiver === friend && aMessages[i].author === user) || (aMessages[i].receiver === user && aMessages[i].author === friend)){
 	    		$("#chat").append("<label id = 'messageAuthor'>" + aMessages[i].author + "</label>");
@@ -90,10 +89,14 @@
 	    }
     }
     function sendMessage(user, text, receiver){
-    	var instance = new message(user, text, receiver);
-    	$("#chat").append("<label id = 'messageAuthor'>" + instance.author + "</label>");
-    	$("#chat").append("<label id = 'messageText'>" + instance.text + "</label>");
-    	messageManager.addNewMessage(instance);
+    	if(receiver){
+	    	var instance = new message(user, text, receiver);
+	    	$("#chat").append("<label id = 'messageAuthor'>" + instance.author + "</label>");
+	    	$("#chat").append("<label id = 'messageText'>" + instance.text + "</label>");
+	    	messageManager.addNewMessage(instance);
+	    } else {
+	    	alert("Pleae choose a chat");
+	    }
     }
 
     /*------------------------------------------------------------------------*/
@@ -121,5 +124,53 @@
 	  			memory.clear();
 	  			location.reload();
 	  		}
+	  	});
+	  	$('#smile').click(function(){
+	  		sendMessage(userName, '<img id="smile" src="img/smiles.png">', $('.single.active').text());
+	  	});
+	  	$('#happy').click(function(){
+	  		sendMessage(userName, '<img id="happy" src="img/smiley_happy.png">', $('.single.active').text());
+	  	});
+	   $('#bus').click(function(){
+	  		sendMessage(userName, '<img id="bus" src="img/smiley_bus.png">', $('.single.active').text());
+	  	});
+	  	$('#blink').click(function(){
+	  		sendMessage(userName, '<img id="blink" src="img/smiley_blink.png">', $('.single.active').text());
+	  	});
+	  	$('#crying').click(function(){
+	  		sendMessage(userName, '<img id="crying" src="img/smiley_crying.png">', $('.single.active').text());
+	  	});
+	  	$('#disappointed').click(function(){
+	  		sendMessage(userName, '<img id="disappointed" src="img/smiley_disappointed.png">', $('.single.active').text());
+	  	});
+	  	$('#laugh').click(function(){
+	  		sendMessage(userName, '<img id="laugh" src="img/smiley_laugh.png">', $('.single.active').text());
+	  	});
+	  	$('#broken-heart').click(function(){
+	  		sendMessage(userName, '<img id="broken-heart" src="img/broken-heart.png">', $('.single.active').text());
+	  	});
+	    $('#sleeping').click(function(){
+	  		sendMessage(userName, '<img id="sleeping" src="img/smiley_sleeping.png">', $('.single.active').text());
+	  	});
+	  	$('#cap').click(function(){
+	  		sendMessage(userName, '<img id="cap" src="img/smiley_cap.png">', $('.single.active').text());
+	  	});
+	  	$('#thumsup').click(function(){
+	  		sendMessage(userName, '<img id="thumsup" src="img/smiley_thumsup.png">', $('.single.active').text());
+	  	});
+	  	$('#clown').click(function(){
+	  		sendMessage(userName, '<img id="clown" src="img/smiley_clown.png">', $('.single.active').text());
+	  	});
+	  	$('#bored').click(function(){
+	  		sendMessage(userName, '<img id="bored" src="img/smiley_bored.png">', $('.single.active').text());
+	  	});
+	  	$('#pleasure').click(function(){
+	  		sendMessage(userName, '<img id="pleasure" src="img/smiley_pleasure.png">', $('.single.active').text());
+	  	});
+	  	$('#humor').click(function(){
+	  		sendMessage(userName, '<img id="humor" src="img/smiley_humor.png">', $('.single.active').text());
+	  	});
+	  	$('#sun').click(function(){
+	  		sendMessage(userName, '<img id="sun" src="img/smiley_sun.png">', $('.single.active').text());
 	  	});  
 	});
